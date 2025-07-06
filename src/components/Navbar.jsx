627dd9142deb3659f8dbf85db2b2929b5fbd373d
Pedro Zich <zichpedro22@gmail.com>
2025-07-06 12:14:47 -0300

    fix: update Navbar link to point to dashboard

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
-            <Link to="/" className="flex items-center space-x-2">
+            <Link to="/dashboard" className="flex items-center space-x-2">
               <div className="w-8 h-8 bg-brown-600 rounded-lg flex items-center justify-center">
                 <Heart className="w-5 h-5 text-white" />
               </div>
