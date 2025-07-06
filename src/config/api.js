5b776867940a476f438b87fbe4c14f8df147c037
Pedro Zich <zichpedro22@gmail.com>
2025-07-06 16:21:13 -0300

    refactor: streamline request interceptor for cleaner code

--- src/config/api.js
+++ src/config/api.js
@@ -11,28 +11,12 @@ const apiClient = axios.create({
   },
 });
 
-apiClient.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("authToken");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
+apiClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem("authToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
-);
-
-apiClient.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem("authToken");
-      window.location.href = "/login";
-    }
-    return Promise.reject(error);
-  }
-);
+  return config;
+});
 
 export default apiClient;
